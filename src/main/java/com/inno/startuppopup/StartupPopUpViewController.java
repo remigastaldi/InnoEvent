c84cc5f6ea7ce1f8f2711c5d70665b7cad37edf7
hubert_i <leohub@live.fr>
2018-10-09 18:12:49 -0400

    Add popupView

---
+++ src/main/java/com/inno/startuppopup/StartupPopUpViewController.java
@@ -0,0 +1,28 @@
+/*
+ * File Created: Wednesday, 26th September 2018
+ * Author: GASTALDI Rémi
+ * -----
+ * Last Modified: Thursday, 4th October 2018
+ * Modified By: GASTALDI Rémi
+ * -----
+ * Copyright - 2018 GASTALDI Rémi
+ * <<licensetext>>
+ */
+
+package com.inno.startuppopup;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+public class StartupPopUpViewController {
+  @FXML
+  private Button testButton;
+
+  public StartupPopUpViewController() {
+  }
+
+	@FXML
+	private void initialize() {
+  }
+}
