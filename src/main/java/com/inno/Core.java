130f5cb871561641f2ffa514a7cbe3e6ea5144ea
Remi Gastaldi <remi.gastaldi@epitech.eu>
2018-10-10 11:44:28 -0400

    Add service and core class

---
+++ src/main/java/com/inno/Core.java
@@ -0,0 +1,19 @@
+/*
+ * File Created: Tuesday, 9th October 2018
+ * Author: GASTALDI Rémi
+ * -----
+ * Last Modified: Wednesday, 10th October 2018
+ * Modified By: GASTALDI Rémi
+ * -----
+ * Copyright - 2018 GASTALDI Rémi
+ * <<licensetext>>
+ */
+
+
+package com.inno;
+
+public class Core {
+  Core() {
+
+  }
+}
---
+++ src/main/java/com/inno/service/View.java
@@ -0,0 +1,19 @@
+/*
+ * File Created: Wednesday, 10th October 2018
+ * Author: GASTALDI Rémi
+ * -----
+ * Last Modified: Wednesday, 10th October 2018
+ * Modified By: GASTALDI Rémi
+ * -----
+ * Copyright - 2018 GASTALDI Rémi
+ * <<licensetext>>
+ */
+
+
+package com.inno.service;
+
+public class View {
+  View() {
+
+  }
+}
