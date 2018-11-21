38f9f4b0957caee5af679c782e63fe3616b38eb4
hubert_i <leohub@live.fr>
2018-11-21 18:28:54 -0500

    Add validator

--- src/main/java/com/inno/ui/popup/StartupPopupNewProjectViewController.java
+++ src/main/java/com/inno/ui/popup/StartupPopupNewProjectViewController.java
@@ -2,7 +2,7 @@
  * File Created: Friday, 12th October 2018
  * Author: HUBERT Léo
  * -----
- * Last Modified: Tuesday, 20th November 2018
+ * Last Modified: Wednesday, 21st November 2018
  * Modified By: HUBERT Léo
  * -----
  * Copyright - 2018 HUBERT Léo
@@ -13,14 +13,17 @@ package com.inno.ui.popup;
 
 import javafx.fxml.FXML;
 
-
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.TextField;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.inno.ui.Validator;
 import com.inno.ui.ViewController;
 import com.inno.ui.View.AnimationDirection;
-
 
 public class StartupPopupNewProjectViewController extends ViewController {
   @FXML
@@ -43,7 +46,6 @@ public class StartupPopupNewProjectViewController extends ViewController {
   private TextField vital_space_width_input;
   @FXML
   private TextField vital_space_height_input;
-  
 
   public void init() {
   }
@@ -54,6 +56,11 @@ public class StartupPopupNewProjectViewController extends ViewController {
 
   @FXML
   private void doneButtonAction() {
+
+    if (checkInputs(true) == false) {
+      System.out.println("EEEE");
+      return;
+    }
     System.out.println(project_name_input.getText());
 
     Double roomWidth = Double.parseDouble(room_width_input.getText());
@@ -64,22 +71,49 @@ public class StartupPopupNewProjectViewController extends ViewController {
     Double vitalSpaceHeight = Double.parseDouble(vital_space_height_input.getText());
 
     System.out.println(roomWidth);
-    double[] scenePos = { roomWidth / 2 - sceneWidth / 2,
-                          roomHeight / 2 - sceneHeight / 2,
-                          roomWidth / 2 + sceneWidth / 2,
-                          roomHeight / 2 - sceneHeight / 2,
-                          roomWidth / 2 + sceneWidth / 2,
-                          roomHeight / 2 + roomHeight / 2,
-                          roomWidth / 2 - sceneWidth / 2,
-                          roomHeight / 2 + roomHeight / 2 };
+    double[] scenePos = { roomWidth / 2 - sceneWidth / 2, roomHeight / 2 - sceneHeight / 2,
+        roomWidth / 2 + sceneWidth / 2, roomHeight / 2 - sceneHeight / 2, roomWidth / 2 + sceneWidth / 2,
+        roomHeight / 2 + roomHeight / 2, roomWidth / 2 - sceneWidth / 2, roomHeight / 2 + roomHeight / 2 };
 
     Core().createRoom(project_name_input.getText(), roomWidth, roomHeight, vitalSpaceWidth, vitalSpaceHeight);
-    
+
     Core().createScene(sceneWidth, sceneHeight, scenePos);
-    
-      // Core().setVitalS(Integer.parseInt(room_height_input.getText()));
+
+    // Core().setVitalS(Integer.parseInt(room_height_input.getText()));
 
     View().showMainView();
+  }
+
+  @FXML
+  private void onKeyReleased() {
+    checkInputs(false);
+  }
+
+  private boolean checkInputs(boolean required) {
+    boolean valid = true;
+
+    HashMap<TextField, String> fields = new LinkedHashMap<>();
+    fields.put(project_name_input, required ? "required|" : "" + "max:30");
+    fields.put(room_width_input, required ? "required|" : "" + "numeric");
+    fields.put(room_height_input, required ? "required|" : "" + "numeric");
+    fields.put(scene_width_input, required ? "required|" : "" + "numeric");
+    fields.put(scene_height_input, required ? "required|" : "" + "numeric");
+    fields.put(vital_space_width_input, required ? "required|" : "" + "numeric");
+    fields.put(vital_space_height_input, required ? "required|" : "" + "numeric");
+
+    for (Map.Entry<TextField, String> entry : fields.entrySet()) {
+      TextField field = entry.getKey();
+      String validator = entry.getValue();
+      if ((required || field.isFocused()) && !Validator.validate(field.getText(), validator)) {
+        valid = false;
+        if (!field.getStyleClass().contains("error"))
+          field.getStyleClass().add("error");
+      } else if (field.isFocused()) {
+        if (field.getStyleClass().contains("error"))
+          field.getStyleClass().remove("error");
+      }
+    }
+    return valid;
   }
 
   @FXML
