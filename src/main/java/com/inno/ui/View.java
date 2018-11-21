a90ac7e62f73b8fcec4d679b0621cdf10bc9b7cb
hubert_i <leohub@live.fr>
2018-11-21 10:46:05 -0500

    Add header sidebar colors & fix popup resizable

--- src/main/java/com/inno/ui/View.java
+++ src/main/java/com/inno/ui/View.java
@@ -2,7 +2,7 @@
  * File Created: Wednesday, 10th October 2018
  * Author: GASTALDI Rémi
  * -----
- * Last Modified: Saturday, 17th November 2018
+ * Last Modified: Wednesday, 21st November 2018
  * Modified By: HUBERT Léo
  * -----
  * Copyright - 2018 GASTALDI Rémi
@@ -26,6 +26,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.AnchorPane;
 import com.inno.ui.innoengine.InnoEngine;
+import javafx.stage.Modality;
 
 public class View extends Application {
 
@@ -79,7 +80,9 @@ public class View extends Application {
       view.setView(this);
       view.addIntent(intent);
       view.init();
+      stage.setResizable(false);
       stage.setScene(scene);
+      stage.initModality(Modality.APPLICATION_MODAL);
       stage.setAlwaysOnTop(true);
       stage.show();
     } catch (Exception e) {
