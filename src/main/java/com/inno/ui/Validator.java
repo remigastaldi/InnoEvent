656b95ca2699a2c9e556488cfae868072dcc48cb
hubert_i <leohub@live.fr>
2018-11-22 13:10:11 -0500

    Inprove Validator

--- src/main/java/com/inno/ui/Validator.java
+++ src/main/java/com/inno/ui/Validator.java
@@ -1,58 +1,75 @@
+/*
+ * File Created: Wednesday, 21st November 2018
+ * Author: HUBERT Léo
+ * -----
+ * Last Modified: Thursday, 22nd November 2018
+ * Modified By: HUBERT Léo
+ * -----
+ * Copyright - 2018 HUBERT Léo
+ * <<licensetext>>
+ */
+
 package com.inno.ui;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Validator {
 
     Validator() {
     }
 
+    interface ValidatorFunction<T, U, R> {
+        R apply(T t, U u);
+    }
+
     public static boolean validate(String var, String validators) {
-        boolean valid = true;
-        if (!validators.contains("required") && var.trim().length() == 0) {
-            return true;
-        }
-        String[] validatorsData = validators.split("\\|", -1);
-        for (String data : validatorsData) {
-            if (data.trim().length() != 0) {
-                String[] validatorData = data.split("\\:", -1);
-                switch (validatorData[0]) {
-                case "required":
-                    if (var.length() == 0 || var.trim().length() == 0) {
-                        valid = false;
-                    }
-                    break;
-                case "numeric":
-                    if (!isNumeric(var)) {
-                        valid = false;
-                    }
-                    break;
-                case "double":
-                    if (!isDouble(var)) {
-                        valid = false;
-                    }
-                    break;
-                case "integer":
-                    if (!isInteger(var)) {
-                        valid = false;
-                    }
-                    break;
-                case "max":
-                    if (validatorData.length <= 1 || !isInteger(validatorData[1])
-                            || var.length() > new Integer(validatorData[1])) {
-                        valid = false;
-                    }
-                    break;
-                case "min":
-                    if (validatorData.length <= 1 || !isInteger(validatorData[1])
-                            || var.length() < new Integer(validatorData[1])) {
-                        valid = false;
-                    }
-                    break;
-                default:
-                    break;
+
+        if (validators.contains("required") || var.trim().length() != 0) {
+            HashMap<String, ValidatorFunction<String, String, Boolean>> checkers = new LinkedHashMap<>();
+            checkers.put("required", (v, n) -> {
+                return !(v.trim().length() == 0);
+            });
+            checkers.put("numeric", (v, n) -> {
+                return isNumeric(v);
+            });
+            checkers.put("double", (v, n) -> {
+                return isDouble(v);
+            });
+            checkers.put("integer", (v, n) -> {
+                return isInteger(v);
+            });
+            checkers.put("max", (v, n) -> {
+                if (n == null || !isInteger(n))
+                    return false;
+                Integer newN = new Integer(n);
+                return isInferiorOf(v, newN, !validators.contains("numeric"));
+            });
+
+            String[] validatorsTab = validators.split("\\|", -1);
+            for (String validator : validatorsTab) {
+                String[] validatorTab = validator.split("\\:", -1);
+                String validatorData = validatorTab.length <= 1 ? null : validatorTab[1];
+                validator = validatorTab[0];
+
+                if (checkers.containsKey(validator) && checkers.get(validator).apply(var, validatorData) == false) {
+                    return false;
                 }
             }
         }
-        return valid;
+        return true;
+    }
+
+    public static boolean isInferiorOf(String var, Integer maxValue, boolean isString) {
+        if (isString && var.length() > maxValue) {
+            return false;
+        } else if (!isString && isNumeric(var)) {
+            Integer newVar = new Integer(var);
+            if (newVar > maxValue) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static boolean isNumeric(String var) {
